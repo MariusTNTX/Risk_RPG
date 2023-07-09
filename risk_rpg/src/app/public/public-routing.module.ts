0a2920267e311d183dbb2df992396ab43cacb969
MariusTNTX <molinamario.msc@gmail.com>
2023-07-10 00:08:54 +0200

    Corrección dimensiones home

--- risk_rpg/src/app/public/public-routing.module.ts
+++ risk_rpg/src/app/public/public-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     component: PublicComponent,
     children: [
       {
-        path: 'home',
+        path: '',
         component: HomeComponent,
       },
       {
--- risk_rpg/src/app/public/public.module.ts
+++ risk_rpg/src/app/public/public.module.ts
@@ -5,6 +5,7 @@ import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MapManagerComponent } from './components/map-manager/map-manager.component';
@@ -25,6 +26,7 @@ import { GameListComponent } from './components/home/game-list/game-list.compone
   imports: [
     CommonModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatToolbarModule, 
     PublicRoutingModule,

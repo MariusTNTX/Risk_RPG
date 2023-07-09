9e2aa69b5d156c4820536f82d79d5c3251a33ab3
MariusTNTX <molinamario.msc@gmail.com>
2023-07-09 22:01:31 +0200

    Estructura general e inicio de sección principal 2

---
+++ risk_rpg/src/app/public/public-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public.component';
+import { MapManagerComponent } from './components/map-manager/map-manager.component';
+import { NewGameComponent } from './components/new-game/new-game.component';
+import { HomeComponent } from './components/home/home.component';
+
+//TODO En la ruta '' no muestra Home
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'mapManager',
+        component: MapManagerComponent,
+      },
+      {
+        path: 'newGame',
+        component: NewGameComponent,
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PublicRoutingModule { }
---
+++ risk_rpg/src/app/public/public.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PublicRoutingModule } from './public-routing.module';
+import { PublicComponent } from './public.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MapManagerComponent } from './components/map-manager/map-manager.component';
+import { NewGameComponent } from './components/new-game/new-game.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapListComponent } from './components/home/map-list/map-list.component';
+import { GameListComponent } from './components/home/game-list/game-list.component';
+
+@NgModule({
+  declarations: [
+    PublicComponent,
+    MapManagerComponent,
+    NewGameComponent,
+    HomeComponent,
+    MapListComponent,
+    GameListComponent
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule, 
+    PublicRoutingModule,
+  ]
+})
+export class PublicModule { }
